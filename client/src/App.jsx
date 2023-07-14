1b3cf3b95d2e7edb21fa8a566ec4a0b11f98c25d
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-14 10:04:29 +0530

    Admin First

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import SignUp from './pages/user/Signup';
 import Login from './pages/user/Login';
 import Home from './pages/user/home';
-import Admin from './pages/admin/Admin';
+import Admin from './pages/admin/admin';
 
 
+// Custom component to validate admin access
+const AdminRouteGuard = () => {
+  const user = JSON.parse(localStorage.getItem('currentUser'));
 
+  if (user && user.isAdmin) {
+    return <Admin />;
+  } else {
+    // Redirect to homepage or any other page
+    return <Navigate to="/home" />;
+  }
+};
 
 
 function App() {
@@ -16,8 +26,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} exact />
           <Route path="/login" element={<Login />} exact />
           <Route path="/home" element={<Home />} exact />
-          <Route path="/admin" element={<Admin />} exact />
-          <Route path="/admin:tab" element={<Admin />} exact />
+          <Route path="/admin/*" element={<AdminRouteGuard />} />
         </Routes>
       </BrowserRouter>
     </div>
